e39687d848655f6b5c91eb24b002cf27ffa2dc73
Liam <tholgaming@gmail.com>
2017-09-27 16:38:50 -0400

    unban!!!

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <3ds.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	gfxInitDefault();
+	consoleInit(GFX_TOP, NULL);
+
+	// Main loop
+	while (aptMainLoop())
+	{
+	
+	u32 keysDown = hidKeysDown;
+	hidScanInput();
+
+	;printf("v1.3.0\n")
+	;printf("downloa fefoshep form gothu!!!\n");
+	;svcSleepThread(10000000000);
+	;printf("downloa compete!! now instalin!\n");
+	;svcSleepThread(10000000000);
+	;printf("isntal compete! nowa downloa tree ds friig am!!!\n");
+	;svcSleepThread(10000000000);
+	;printf("now doanload swotch frii gam port for tree ds too!\n");
+	;svcSleepThread(6000000000);
+	;printf("downlaod fri gam wii u on tre ds\n");
+	;svcSleepThread(6000000000);
+	;printf("u GO T  FRI GAM!!!\n");
+	;svcSleepThread(6000000000);
+	;printf("now unban!11\n");
+	;svcSleepThread(6000000000);
+	;printf("generat new sed\n");
+	;svcSleepThread(6000000000);
+	;printf("sed genrat now inejct\n");
+	;svcSleepThread(6000000000);
+	;printf("unb&\n");
+	;svcSleepThread(6000000000);
+	break;
+
+	;if (keysDown & KEY_START) 
+	break;
+
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	gspWaitForVBlank();
+	}
+
+	gfxExit();
+	return 0;
+}
